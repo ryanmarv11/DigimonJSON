787f73491c4d2aea11f64fc780bc2e5080dcb615
ryanmarv11 <ryan@DESKTOP-O5NT9CN.localdomain>
2022-01-02 00:55:47 -0500

    another step forward

--- jsonMaker.py
+++ jsonMaker.py
@@ -13,59 +13,158 @@ Quit
 
 """
 
-
 import json
 
-globalNumber = 1
+digimonCounter = 1
+
+class DigimonList:
+	def __init__(self):
+		self.digimonList = []
+
+	def getDigimonList(self):
+		return self.digimonList
+
+	def printDigimonList(self):
+		if self.length() == 0:
+			print("Empty list.")
+		else:
+			for i in range(self.length()):
+				print(str(i) + ":\t" + self.digimonList[i].toString())
+
+	def delete(self, toDelete):
+		self.digimonList.pop(toDelete)
+		i = toDelete
+		for i in range(len(self.digimonList)):
+			self.digimonList[i].setCardNumber(self.digimonList[i].getCardNumber() - 1)
+		global digimonCounter
+		digimonCounter -= 1
+
+	def insert(self):
+		name = input("Name?")
+		rarity = input("Rarity?")
+		self.digimonList.append(Digimon(name, rarity))
+
+	def length(self):
+		return len(self.digimonList)
 
 
-def insert(currentList):
-	newEntry = []
-	name = input("Name?")
-	newEntry.append(name)
-	cardNumber = globalNumber
-	globalNumber += 1
-	name = input("Rarity?")
+
+class Digimon:
+	def __init__(self, name, rarity):
+		self.name = name 
+		self.rarity = rarity
+		global digimonCounter
+		self.cardNumber = digimonCounter
+		digimonCounter += 1
+
+	def setName(self, newName):
+		self.name = newName
+
+	def setRarity(self, newRarity):
+		self.rarity = newRarity
+
+	def setCardNumber(self, newCardNumber):
+		self.cardNumber = newCardNumber
+
+	def getName(self):
+		return self.name 
+
+	def getRarity(self):
+		return self.rarity
+
+	def getCardNumber(self):
+		return self.cardNumber
+
+	def toString(self):
+		return "Name: " + self.name + "\tRarity: " + self.rarity + "\tCardNumber: " + str(self.cardNumber)
+
+	def toJSONObject(self):
+		return "{\"cardNumber\": " + str(self.cardNumber) + ",\"name\": \"" + self.name + "\",\"rarity\": \"" + self.rarity + "\"},\n"
+
+	def digimonEditMenu(self):
+		print(self.toString())
+		print("Edit Digimon:")
+		print("0: Edit name")
+		print("1: Edit rarity")
+		print("2: Edit number")
+		command = input()
+		if command == '0':
+			self.name = input("New Name:")
+		elif command == '1':
+			self.rarity = input("New Rarity:")
+		elif command == '2':
+			self.cardNumber = int(input("New CardNumber"))
+		return self
+
+
+
 
 def editNumberCommand():
-	print("globalNumber is " + str(globalNumber))
-	editNumberCommand = input("What number should globalNumber be set to?")
+	print("digimonCounter is " + str(digimonCounter))
+	editNumberCommand = input("What number should digimonCounter be set to?")
 	check = input("Are you sure you want it to be " + str(editNumberCommand))
 	while check == 'no':
-		print("globalNumber is " + str(globalNumber))
-		editNumberCommand = input("What number should globalNumber be set to?")
+		print("digimonCounter is " + str(digimonCounter))
+		editNumberCommand = input("What number should digimonCounter be set to?")
 		check = input("Are you sure you want it to be " + str(editNumberCommand))
 
 
+def clearScreen():
+	for i in range(20):
+		print()
+
+
 def main():
-	fileName = input("Input set name: ")
-	fileName = "/mnt/c/Users/ryanm/Documents/Coding/DigimonJSON/" + fileName + ".json"
-	print(fileName)
-	try:
-		verifyFile = open(fileName, "r")
-	except FileNotFoundError:
-		print("File not found")
-		return
-
-	f = open(fileName, "a")
-	f.write("{\n")
-	f.write("\t\"data\":\n")
-	f.write("[\n")
-
-	size = input("How many cards are in the set?")
-	currentList = []
+	dList = DigimonList()
 	while True:
+		print("Main Menu:")
+		print("0: Print currentList")
+		print("1: Print digimonCounter")
+		print("2: Edit digimonCounter")
+		print("3: Insert Digimon")
+		print("4: Edit Digimon")
+		print("q: Quit")
 
-		command = input("Input your command: i is for Insert, d is for delete, e is for edit, p is for print, q is for quit")
-		if command == 'i':
-			insert(currentList)
-			break
-		elif command == 'edit number':
+		command = input()
+		if command == '0':
+			clearScreen()
+			dList.printDigimonList()
+		elif command == '1':
+			clearScreen()
+			print(digimonCounter)
+		elif command == '2':
+			clearScreen()
 			editNumberCommand()
+		elif command == '3':
+			clearScreen()
+			dList.insert()
+		elif command == '4':
+			clearScreen()
+			dList.printDigimonList()
+			index = int(input("Which entry would you like to edit?"))
+			if 0 <= index and dList.length() > index:
+				dList.digimonList[index].digimonEditMenu()
 		elif command == 'q':
 			break
 
+	return dList 
 
+def digimonListToJSONString(dList):
+	toReturn = "{\n\t\"data\":\n\t[\n\t\t"
+	for item in dList.digimonList:
+		toReturn += item.toJSONObject()
+	toReturn += "\t]\n}"
+	return toReturn		
 
-
-main()
+toJson = main()
+jsonString = digimonListToJSONString(toJson)
+fileName = input("Input set name: ")
+fileName = "/mnt/c/Users/ryanm/Documents/Coding/DigimonJSON/" + fileName + ".json"
+print(fileName)
+try:
+	verifyFile = open(fileName, "r")
+except FileNotFoundError:
+	print("File not found")
+	
+f = open(fileName, "a")
+f.write(jsonString)
